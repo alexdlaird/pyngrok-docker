6495daf15c96c1f7b1bd6f76f39be827b62c5939
Alex Laird <contact@alexlaird.com>
2025-06-02 12:10:18 -0500

    Paramterize build, publish.

--- build.sh
+++ build.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
 LINUX_PLATFORMS="linux/386,linux/amd64,linux/arm,linux/arm64,linux/ppc64le,linux/s390x"
-WINDOWS_PLATFORMS="windows/amd64"
 
-# Python 3.12 - Alpine
-docker buildx build --target alpine_3_12 -t alexdlaird/pyngrok:python-3.12-alpine --platform=$LINUX_PLATFORMS 7/3.12/alpine
+for PYTHON_VERSION in 3.8 3.9 3.10 3.11 3.12 3.13; do
+  for DISTRO in bookworm bullseye alpine; do
+    docker buildx build \
+        -t alexdlaird/pyngrok:$PYTHON_VERSION-$DISTRO \
+        --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+        --build-arg DISTRO=$DISTRO \
+        --platform=$LINUX_PLATFORMS \
+        linux
 
-# Python 3.12 - Windows
-#docker buildx build --target windows_3_12 -t alexdlaird/pyngrok:python-3.12-windows --platform=$WINDOWS_PLATFORMS 7/3.12/windows
+    # Tag a default distro, for when none is given
+    if DISTRO == "bookworm"; then
+      IMAGE_ID=$(docker images -q --format '{{.ID}}' | head -1)
+      docker tag $IMAGE_ID alexdlaird/pyngrok:$PYTHON_VERSION
+
+      if PYTHON_VERSION == "3.13"; then
+        docker tag $IMAGE_ID alexdlaird/pyngrok
+      fi
+    fi
+  done
+
+  # Tag  default Python version, for when none is given
+  if PYTHON_VERSION == "3.13"; then
+    IMAGE_ID=$(docker images -q --format '{{.ID}}' | head -1)
+    docker tag $IMAGE_ID alexdlaird/pyngrok:$DISTRO
+  fi
+done
