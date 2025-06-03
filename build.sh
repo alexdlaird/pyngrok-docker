368a762c886b34baa81737deca1b1aea224a8c1a
Alex Laird <contact@alexlaird.com>
2025-06-02 20:03:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -x
 
 DOCKER_USERNAME="${DOCKER_USERNAME:-alexdlaird}"
 
 if [[ "$PYTHON_VERSION" == "" ]]; then echo "PYTHON_VERSION is not set" & exit 1 ; fi
 if [[ "$DISTRO" == "" ]]; then echo "DISTRO is not set" & exit 1 ; fi
 if [[ "$PLATFORM" == "" ]]; then echo "PLATFORM is not set" & exit 1 ; fi
+
+PUBLISH_ARGS=""
 if [[ "$PUBLISH" != "" ]]; then
   if [[ "$DOCKER_ACCESS_TOKEN" == "" ]]; then echo "DOCKER_ACCESS_TOKEN is not set" & exit 1 ; fi
+else
+  echo "$DOCKER_ACCESS_TOKEN" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+  PUBLISH_ARGS+=" --push"
+fi
+
+# Build tag aliases
+ADDITIONAL_TAGS=""
+if [[ "$PYTHON_VERSION" == "3.13" ]]; then
+  ADDITIONAL_TAGS+=" -t $DOCKER_USERNAME/pyngrok:$DISTRO"
+
+  if [[ "$DISTRO" == "bookworm" ]]; then
+    ADDITIONAL_TAGS+=" -t $DOCKER_USERNAME/pyngrok:$PYTHON_VERSION"
+    ADDITIONAL_TAGS+=" -t $DOCKER_USERNAME/pyngrok:latest"
+  fi
 fi
 
 docker buildx build \
     -t "$DOCKER_USERNAME/pyngrok:$PYTHON_VERSION-$DISTRO" \
+    $ADDITIONAL_TAGS \
     --build-arg "PYTHON_VERSION=$PYTHON_VERSION" \
     --build-arg "DISTRO=$DISTRO" \
     --platform="$PLATFORM" \
+    $PUBLISH_ARGS \
     .
-
-# Add special tags for default images
-if [[ "$PYTHON_VERSION" == "3.13" ]]; then
-  IMAGE_ID=$(docker images -q --format '{{.ID}}' | head -1)
-
-  docker tag "$IMAGE_ID" "$DOCKER_USERNAME/pyngrok:$DISTRO"
-
-  if [[ "$DISTRO" == "bookworm" ]]; then
-    docker tag "$IMAGE_ID" "$DOCKER_USERNAME/pyngrok:$PYTHON_VERSION"
-    docker tag "$IMAGE_ID" "$DOCKER_USERNAME/pyngrok:latest"
-  fi
-fi
-
-if [[ "$PUBLISH" != "" ]]; then
-  echo "$DOCKER_ACCESS_TOKEN" | docker login --username "$DOCKER_USERNAME" --password-stdin
-
-  docker push "$DOCKER_USERNAME/pyngrok:$PYTHON_VERSION-$DISTRO"
-fi
