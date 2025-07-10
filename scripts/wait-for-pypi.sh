e141810abdb8bbdb96075bc5da6176aed55a1ec6
Alex Laird <alaird@twilio.com>
2025-07-10 12:13:10 -0500

    Only wait on PyPI if deployment is brand new and <10 mins has passed.

--- scripts/wait-for-pypi.sh
+++ scripts/wait-for-pypi.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 PYTHON_BIN=${PYTHON_BIN:-python}
+DATE_BIN=${DATE_BIN:-date}
 RETRIES="${RETRIES:-30}"
-WAIT_TIME="${WAIT_TIME:-10}"
+RETRY_WAIT_TIME="${RETRY_WAIT_TIME:-10}"
+CACHE_WAIT_TIME="${CACHE_WAIT_TIME:-600}"
 
 if [[ "${VERSION}" == "" ]]; then echo "VERSION is not set" & exit 1 ; fi
 # shellcheck disable=SC2046
@@ -13,15 +15,23 @@ fi
 PYNGROK_VERSION="${VERSION%.*}"
 
 for ((i=0; i<RETRIES; ++i)); do
-  if $PYTHON_BIN -m pip index versions pyngrok | grep -i "Available versions: $PYNGROK_VERSION" > /dev/null; then
+  UPLOAD_TIME=$(curl -s 'https://pypi.org/pypi/pyngrok/json' | jq -r ".releases.\"$PYNGROK_VERSION\"[0].upload_time_iso_8601")
+  if [ "$UPLOAD_TIME" != "null" ]; then
+    UPLOAD_TIME=$($DATE_BIN -d "$UPLOAD_TIME" +%s)
     break
   fi
 
   echo "Waiting for pyngrok==$PYNGROK_VERSION to be available on PyPI ..."
-  sleep "$WAIT_TIME"
+  sleep "$RETRY_WAIT_TIME"
 
   (( RETRY_COUNT+=1 ))
 done
 
-echo "pyngrok==$PYNGROK_VERSION found on PyPI, waiting 10 minutes for cache disbursement ..."
-sleep 600
+echo "pyngrok==$PYNGROK_VERSION found on PyPI"
+CURRENT_TIME=$($DATE_BIN "+%s")
+ELAPSED_TIME=$(($CURRENT_TIME - $UPLOAD_TIME))
+if [ $ELAPSED_TIME -lt $CACHE_WAIT_TIME ]; then
+  NEEDED_WAIT=600-$ELAPSED_TIME
+  echo "Waiting $NEEDED_WAIT more seconds to ensure cache disbursement ..."
+  sleep $NEEDED_WAIT
+fi
